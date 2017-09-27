cde28f8780d074f30084ab9448751f2c6a6721e7
Liz Yu <liz.yu123@gmail.com>
2017-09-27 21:22:58 +1000

    Adding nodename variable

--- check-node-up.py
+++ check-node-up.py
@@ -4,40 +4,32 @@ import calendar, time
 import requests
 import json
 
-def main():
-    #global timestamp
+
+def main(nodename):
+    ''' request puppet node catalog_timestamp
+        on success, check node up within last 30 mins
+    ''' 
     
-    unixboxisup = False
-
-    while not unixboxisup:
-        r = requests.get('http://<api>', auth=('user', 'pass'))
-        # x = """{
-        #         "cached_catalog_status": "not_used",
-        #         "catalog_environment": "production",
-        #         "catalog_timestamp": "2016-08-15T11:06:26.275Z",
-        #         "certname": "greenserver.vm",
-        #         "deactivated": null,
-        #         "expired": null,
-        #         "facts_environment": "production",
-        #         "facts_timestamp": "2016-08-15T11:06:26.140Z",
-        #         "latest_report_corrective_change": null,
-        #         "latest_report_hash": "4a956674b016d95a7b77c99513ba26e4a744f8d1",
-        #         "latest_report_noop": false,
-        #         "latest_report_noop_pending": null,
-        #         "latest_report_status": "changed",
-        #         "report_environment": "production",
-        #         "report_timestamp": "2016-08-15T11:06:18.393Z"
-        #     }
-        # """
-        #d = json.loads(x)
-        d = r.json.loads(r)
-        timestamp_puppet = d["catalog_timestamp"]   # python ver 2.x
-        print(timestamp_puppet)
-        #timestamp_puppet = d["catalog_timestamp"].Value $ ??? python ver 3.x
-        unixboxisup = checkboxisup(timestamp_puppet)
+    while True:
+        try:
+            r = requests.get('http://abcd.tv/{0}'.format(nodename), timeout=1, auth=('user', 'pass'))
+            r.raise_for_status()
+            if r.status_code == 200:
+                d = r.json.loads(r)
+                timestamp_puppet = d["catalog_timestamp"]
+                if comparetimestamp(timestamp_puppet):
+                    dowork()
+                    break
+        except requests.exceptions.HTTPError as err:
+            print err
+        except requests.exceptions.RequestException as e:
+            print e
+        time.sleep(15)
 
 
-def checkboxisup(timestamp_puppet):
+def comparetimestamp(timestamp_puppet):
+    ''' success == node up within last 30 mins
+    ''' 
 
     utc_puppet = time.strptime(timestamp_puppet, "%Y-%m-%dT%H:%M:%S.%fZ")
     utc_local = time.gmtime()
@@ -45,28 +37,15 @@ def checkboxisup(timestamp_puppet):
     epoch_puppet = calendar.timegm(utc_puppet)
     epoch_local = calendar.timegm(utc_local)
 
-    print("epoch puppet ", epoch_puppet)
-    print("epoch local  ", epoch_local)
-
-    if epoch_puppet < epoch_local-1800:
-        return False
-
-    # print("unix box is up %s", (time.localtime))
-    # print("unix box is up {0}".format(time.localtime))
-    return True
+    return epoch_puppet > epoch_local-1800
 
 
-# checkboxisup("2017-09-18T12:20:00.000Z")   # testing manualy
+def dowork():
+    # node is up, do what we need to here
+    print "node is up"
+    pass
+
 
 if __name__ == "__main__":
-    # if len(sys.argv)!=2:
-    #     print("Please pass unix UTC eg. '2017-09-18T12:20:00.000Z'")
-    # else:
-    #     main(sys.argv[1])
-    #     #int("arg[0]={0}".format(sys.argv[0]))
-    #     # print("arg[1]={0}".format(sys.argv[1]))
-    #     # print(">>>>> len(sys.argv) = %i" % len(sys.argv))
-        # pass
-    main()
+    main(sys.argv[1])
     
-
