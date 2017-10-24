b99c3c8251b079be5ad2fe30a0450b362309984d
Liz Yu <liz.yu123@gmail.com>
2017-10-24 22:25:49 +1100

    Rename check-node-up.py to check-node-up-241017.py
    Add check input argument, ensure there is 1 argument pass into the script.

---
+++ check-node-up-241017.py
@@ -0,0 +1,53 @@
+
+import sys
+import os
+import calendar, time
+import requests
+import json
+
+
+def main(nodename):
+    ''' request puppet node catalog_timestamp
+        on success, check node up within last 30 mins
+    ''' 
+    
+    while True:
+        try:
+            r = requests.get('http://abcd.tv/{0}'.format(nodename), timeout=1, auth=('user', 'pass'))
+            r.raise_for_status()
+            if r.status_code == 200:
+                d = r.json.loads(r)
+                timestamp_puppet = d["catalog_timestamp"]
+                if comparetimestamp(timestamp_puppet):
+                    dowork()
+                    break
+        except requests.exceptions.HTTPError as err:
+            print err
+        except requests.exceptions.RequestException as e:
+            print e
+        time.sleep(15)
+
+
+def comparetimestamp(timestamp_puppet):
+    ''' success == node up within last 30 mins
+    ''' 
+
+    utc_puppet = time.strptime(timestamp_puppet, "%Y-%m-%dT%H:%M:%S.%fZ")
+    utc_local = time.gmtime()
+
+    epoch_puppet = calendar.timegm(utc_puppet)
+    epoch_local = calendar.timegm(utc_local)
+
+    return epoch_puppet > epoch_local-1800
+
+
+def dowork():
+    print "node is up"
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        scriptname = sys.argv[0].split('/')[:-1]
+        print("Usage: {0} <hostname>".format(scriptname))
